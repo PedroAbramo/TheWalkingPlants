9b28393386c567c21de5630b1c2e28b4a40e8368
HerculesSP <hercules.pereira@sptech.school>
2025-06-06 12:31:06 -0300

    Atualização exemplo

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 
 var partidaRouter = require("./src/routes/pontuacoes");
-
+var indexRouter = require("./src/routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/partidas", partidaRouter);
-
+app.use("/", indexRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
